d342aa43a3218426b6eb431150d14a891b4e502e
mateogall0 <mateogesede@gmail.com>
2025-08-31 01:56:28 -0300

    wip: wheel of connections

---
+++ include/server/sessions/wheel.h
@@ -0,0 +1,32 @@
+#ifndef ZSERVER_SESSIONS_WHEEL_H
+#define ZSERVER_SESSIONS_WHEEL_H
+
+
+#include <time.h>
+
+
+typedef struct connected_session_s {
+    int client_fd;
+    time_t last_request;
+    time_t start_time;
+    struct connected_session_s *prev;
+    struct connected_session_s *next;
+} connected_session_t;
+
+typedef struct {
+    time_t idle_timout;
+    connected_session_t *head;
+    connected_session_t *tail;
+} connected_sessions_wheel_t;
+
+
+extern connected_sessions_wheel_t *connected_sessions_wheel;
+
+connected_sessions_wheel_t *init_connected_sessions_wheel(time_t idle_timout);
+connected_session_t *push_new_connected_session_towheel(int client_fd);
+connected_session_t *pop_last_connected_session_fromwheel();
+connected_session_t *pop_selected_connected_session_fromwheel(connected_session_t *session);
+void destroy_connected_session(connected_session_t *session);
+void destroy_connected_sessions_wheel();
+
+#endif
---
+++ src/server/sessions/wheel.c
@@ -0,0 +1,54 @@
+#include "server/sessions/wheel.h"
+#include <time.h>
+#include <stdlib.h>
+
+
+connected_sessions_wheel_t *connected_sessions_wheel = NULL;
+
+
+connected_sessions_wheel_t *init_connected_sessions_wheel(time_t idle_timout) {
+    if (connected_sessions_wheel)
+        return (connected_sessions_wheel);
+
+    connected_sessions_wheel = malloc(sizeof(connected_sessions_wheel_t));
+    if (!connected_sessions_wheel)
+        return (NULL);
+
+    connected_sessions_wheel->head = NULL;
+    connected_sessions_wheel->tail = NULL;
+    connected_sessions_wheel->idle_timout = idle_timout;
+
+    return (connected_sessions_wheel);
+}
+
+connected_session_t *push_new_connected_session_towheel(int client_fd) {
+    if (!connected_sessions_wheel)
+        return (NULL);
+
+    connected_session_t *new = malloc(sizeof(connected_session_t));
+    if (!new)
+        return (NULL);
+
+    time_t now = time(NULL);
+
+    new->last_request = now;
+    new->start_time = now;
+    new->client_fd = client_fd;
+    new->next = connected_sessions_wheel->head;
+    new->prev = NULL;
+
+    if (connected_sessions_wheel->head)
+        connected_sessions_wheel->head->prev = new;
+    if (!connected_sessions_wheel->tail)
+        connected_sessions_wheel->tail = new;
+
+    return (new);
+}
+
+connected_session_t *pop_last_connected_session_fromwheel();
+
+connected_session_t *pop_selected_connected_session_fromwheel(connected_session_t *session);
+
+void destroy_connected_session(connected_session_t *session);
+
+void destroy_connected_sessions_wheel();
