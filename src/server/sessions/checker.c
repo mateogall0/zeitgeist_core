b50e37ba8517580cf6a846d10b6a9073cc3e30e9
mateogall0 <mateogesede@gmail.com>
2025-09-03 10:45:20 -0300

    feat: include the session cleaner in the epoll loop

---
+++ src/server/sessions/checker.c
@@ -0,0 +1,24 @@
+#include "server/sessions/wheel.h"
+#include "server/sessions/map.h"
+#include <time.h>
+#include <sys/epoll.h>
+#include <unistd.h>
+
+
+void clear_idle_connection_sessions(int epoll_fd) {
+    if (!connected_sessions_wheel)
+        return;
+    connected_session_t *last;
+    while((last = peek_last_connected_wheel_session())) {
+        if (time(NULL) > last->last_request + connected_sessions_wheel->idle_timout) {
+            int client_fd = last->client_fd;
+            destroy_connected_session(client_fd);
+            if (epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_fd, NULL) == 0) {
+                close(client_fd);
+                destroy_connected_session(client_fd);
+            }
+        }
+        else
+            break;
+    }
+}
